01b3bc6dddbd6a6c5ebbbdfbdc85314d542e976c
ansar798 <62190033+ansar798@users.noreply.github.com>
2024-02-04 16:22:21 -0800

    Update main.py
    Clock add in main.py which show real time with date

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+from datetime import datetime
 print("Hello World!")
+current_time = datetime.now()
+formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+print("Current time:", formatted_time)
 print ("I like your dog.")
